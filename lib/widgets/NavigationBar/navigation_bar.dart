f6362c9bbc673e3e9b886798716a3587df4e6d1c
Mayra <msanchezk17@gmail.com>
2020-11-10 11:52:52 -0800

    added profile image and name
    need to add drop down functionality

--- lib/widgets/NavigationBar/navigation_bar.dart
+++ lib/widgets/NavigationBar/navigation_bar.dart
@@ -18,15 +18,28 @@ class NavigationBar extends StatelessWidget {
             children: <Widget>[
               _NavBarItem('RESTAURANT'),
               SizedBox(
-                width: 50,
+                width: 40,
               ),
               _NavBarItem('REVENUE REPORT'),
               SizedBox(
-                width: 50,
+                width: 40,
               ),
               _NavBarItem('PROFILE'),
               SizedBox(
-                width: 50,
+                width: 40,
+              ),
+              //_NavBarProfile('FName LName'),
+              Image.asset(
+               "assets/pfp.png",
+                height: 30,
+                width: 30,
+              ),
+              SizedBox(width: 5),
+              Text('First Last', style: TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.normal)),
+              //SizedBox( width: 5,),
+              IconButton(
+                icon: Icon(Icons.arrow_drop_down, color: Colors.white, size:25,),
+                onPressed: () {},
               ),
             ],
           )
@@ -36,25 +49,40 @@ class NavigationBar extends StatelessWidget {
   }
 }
 
-//class _NavBarProfile extends StatelessWidget {
-//  final String name;
-//  final String title;
-//  const _NavBarProfile(
-//      this.name, {
-//        Key key, this.title,
-//  }) : super(key: key);
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Text(
-//      title,
-//      style: TextStyle(
-//          color: Colors.white,
-//          fontSize: 16
-//      ),
-//    );
-//  }
-//}
+class _NavBarProfile extends StatelessWidget {
+  final String name;
+  final String title;
+  const _NavBarProfile(
+      this.name, {
+        Key key, this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      child: ListTile(
+        leading: Image.asset(
+         "assets/pfp.png",
+          height: 85,
+          width: 85,
+        ),
+        title: Text(name, style: TextStyle(fontSize: 16)), //employee name
+        subtitle: Text(title, style: TextStyle(fontSize: 12)), //employee title
+        trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children:[
+              IconButton(
+                icon: Icon(Icons.arrow_drop_down, color: Colors.grey, size:25,),
+                onPressed: () {},
+              ),
+            ]),
+      ),
+    );
+  }
+}
+
+
 class _NavBarItem extends StatelessWidget {
   final String title;
   const _NavBarItem(
